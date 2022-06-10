268d02033085220955e1a4cd84f0734162a788b1
abhi3k <abhisheks71@yahoo.com>
2022-06-11 00:00:59 +0530

    class based components

--- react02/src/App.js
+++ react02/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Hello from './component/Hello'
+import HelloClass from './component/HelloClass'
 
 function App() {
   return (
     <div className="App">
-     
+      <Hello />
+      <HelloClass />
     </div>
   );
 }
 
 export default App;
+
+
+/*
+  Class based components - can use state and props
+  Function based components - props ( could not have its own state)
+
+  with the coming of 'Hooks' -> create state is function based component
+*/
---
+++ react02/src/component/HelloClass.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+//Class Based Component
+class HelloClass extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Class</h1>
+            </div>
+        )
+    }
+}
+
+export default HelloClass
