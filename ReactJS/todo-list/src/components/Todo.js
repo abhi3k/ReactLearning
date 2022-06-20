a235722fde99b4271b0f083df0d646a2d883bc16
abhi3k <abhisheks71@yahoo.com>
2022-06-20 19:31:41 +0530

    todo d1

--- ReactJS/todo-list/src/components/Todo.js
+++ ReactJS/todo-list/src/components/Todo.js
@@ -1,35 +1,40 @@
 import React, { Component } from 'react'
-import { Container, Form, Col, InputGroup, Button, ListGroup } from 'react-bootstrap'
+import { Container, Form, Col, Row, InputGroup, Button, ListGroup } from 'react-bootstrap'
 import { FaEdit } from 'react-icons/fa'
-import { FiDelete } from 'react-icon/fi'
+import { MdDeleteOutline, MdOutlineAddBox } from 'react-icons/md'
 
 class Todo extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
-            toDoList : ['revise JavaScript', 'asynchronousJS', 'practice array methods', 'revise react state']
+            toDoList: ['revise JavaScript', 'asynchronousJS', 'practice array methods', 'revise react state']
         }
     }
 
     render() {
         const { toDoList } = this.state
         return (
-            <Container style={{ margin: '20px auto' }}>
+            <Container>
                 <Col md={{ span: 4, offser: 4 }}>
                     <InputGroup className="mb-3">
                         <Form.Control size="lg" type="text" placeholder="Large Text"></Form.Control>
-                        <Button variant="outline-secondary" id="btn-1">+</Button>
+                        <Button variant="outline-secondary" id="btn-1"><MdOutlineAddBox/></Button>
                     </InputGroup>
                     <ListGroup>
                         {
                             // toDoList.
                         }
-                        <ListGroup.Item> Details are here
-                            <Col ms={9}></Col>
-                            <Col ms={3}></Col>
-                            <Button variant="warning"><FaEdit /></Button>
-                            <Button variant="danger">Edit</Button>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col ms={9}>
+                                    Details are here
+                                </Col>
+                                <Col ms={3}>
+                                    <Button variant="warning"><FaEdit /></Button>
+                                    <Button variant="danger"><MdDeleteOutline /></Button>
+                                </Col>
+                            </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
                         <ListGroup.Item>Morbi leo risus</ListGroup.Item>
